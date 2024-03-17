fb7a013c163f2881027d93cbeedaed1dc1e918fc
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2024-03-17 23:17:28 +0200

    tesxFix

--- service/spaceTable.js
+++ service/spaceTable.js
@@ -5,7 +5,7 @@ const createSpaceTable = (total, stuf, license, fix, goods, income) => {
 export const spaceTable = [
   createSpaceTable(48, 7, 10, 5, 26, 2500),
   createSpaceTable(99, 15, 10, 12, 62, 3500),
-  createSpaceTable(210, 45, 10, 30, 125, 3500),
+  createSpaceTable(210, 45, 10, 30, 125, 6500),
   createSpaceTable(310, 60, 10, 45, 185, 9500),
   createSpaceTable(400, 80, 10, 60, 250, 13000),
 ];
