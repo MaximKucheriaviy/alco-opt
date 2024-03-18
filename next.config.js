f7fdd53109ef6b84e09d5e6b95a3be4d8c881e26
Maxim Kucheriaviy <maxim-k@i.ua>
2024-03-18 10:23:30 +0200

    netlyfy fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
+  //basePath: "/",
+  // assetPrefix: "./",
 };
 
 module.exports = nextConfig;
