cceb58c288f18d7c3e9f56550be2a2814c991f5b
Maxim Kucheriaviy <maxim-k@i.ua>
2024-03-28 10:17:47 +0200

    header visual ready

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,23 +3,39 @@ import Link from "next/link";
 import Image from "next/image";
 import { Container } from "../Container/Container";
 import { Button } from "../Button/Button";
-import { useWidth } from "@/service/useDimentions";
+import { useState } from "react";
 
 export const Header = () => {
-  const width = useWidth();
+  const [linkHidden, setLinkHidden] = useState(true);
+  const linkClose = () => {
+    if (window.innerWidth >= 1280) {
+      return;
+    }
+    setLinkHidden((prev) => !prev);
+  };
   return (
     <StyledHeader>
-      <Container>
+      <Container className="headerContainer">
         <div className="blackDiv">
           <Link className="leftLink" href={"/"}>
             <Image src={"/pinSVG.svg"} width={13} height={16} alt="icon" />
             Київ, вул. О. Мишуги, 3А
           </Link>
-          <button className="leftLinkMobile">
+          <button onClick={linkClose} className="leftLinkMobile">
             <Image src={"/pinSVG.svg"} width={20} height={24} alt="icon" />
-            {/* Київ, вул. О. Мишуги, 3А */}
           </button>
-          <div className="rightLinks">
+          <div className={`hiddenMobileLink ${linkHidden && "visuallyHidden"}`}>
+            <Link href={"/"}>Київ, вул. О. Мишуги, 3А </Link>
+            <button onClick={linkClose}>
+              <Image
+                width={12}
+                height={12}
+                src={"/crostRed.svg"}
+                alt="corost"
+              />
+            </button>
+          </div>
+          <div className={`rightLinks ${linkHidden || "visuallyHidden"}`}>
             <Image src={"/instagram.svg"} width={26} height={26} alt="icon" />
             <Image src={"/facebook.svg"} width={26} height={26} alt="icon" />
             <Image src={"/telegram.svg"} width={26} height={26} alt="icon" />
@@ -32,13 +48,29 @@ export const Header = () => {
           </div>
         </div>
         <div className="absoluteDiv">
-          <Image src={"/logo.png"} width={100} height={74} alt="logo" />
+          <div className="logoThumb">
+            <Image
+              src={"/logo.png"}
+              className="imageThumbed"
+              width={100}
+              height={74}
+              alt="logo"
+            />
+          </div>
           <nav className="mainNavigation">
             <Link href={"/"}>Про нас</Link>
             <Link href={"/"}>Програма лояльності</Link>
             <Link href={"/"}>Контакти</Link>
           </nav>
           <Button className={"button"}>Франчайзинг</Button>
+          <button className="burgerButton">
+            <Image
+              src={"/burgerButton.svg"}
+              width={32}
+              height={24}
+              alt="bugerButton"
+            />
+          </button>
         </div>
       </Container>
     </StyledHeader>
--- components/Header/StyledHeader.jsx
+++ components/Header/StyledHeader.jsx
@@ -2,11 +2,20 @@ import styled from "@emotion/styled";
 import { mediaScreen } from "@/service/mediaRules";
 
 export const StyledHeader = styled.header`
+  .headerContainer {
+    width: auto;
+    padding-left: 16px;
+    padding-right: 16px;
+    ${mediaScreen(1280)} {
+      width: 1140px;
+      padding: 0;
+    }
+  }
   .blackDiv {
     height: 42px;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    /* justify-content: space-between; */
   }
   .leftLink {
     display: none;
@@ -32,24 +41,59 @@ export const StyledHeader = styled.header`
     background-color: transparent;
   }
 
+  .hiddenMobileLink {
+    font-family: "Proxima Nova";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-left: 36px;
+    & a {
+      color: #ffffff;
+      text-decoration: none;
+    }
+    & button {
+      background-color: transparent;
+      border: none;
+    }
+  }
   .rightLinks {
     display: flex;
     gap: 18px;
+    margin-left: auto;
   }
   & .absoluteDiv {
     position: absolute;
     left: 0;
-    top: calc(100% + 24px);
+    top: calc(100% + 10px);
+    padding-left: 16px;
+    padding-right: 16px;
+    ${mediaScreen(1280)} {
+      top: calc(100% + 24px);
+      padding: 0px;
+    }
     /* background-color: red; */
     width: 100%;
     z-index: 1000;
     display: flex;
     align-items: center;
-    & .mainNavigation {
-      display: flex;
-      margin-left: 288px;
-      gap: 54px;
+    & .logoThumb {
+      flex-shrink: 0;
+      width: 64px;
+      height: 48px;
 
+      ${mediaScreen(1280)} {
+        width: auto;
+        height: auto;
+      }
+    }
+    & .mainNavigation {
+      display: none;
+      ${mediaScreen(1280)} {
+        display: flex;
+        margin-left: 288px;
+        gap: 54px;
+      }
       a {
         color: #fff;
 
@@ -62,8 +106,20 @@ export const StyledHeader = styled.header`
         text-decoration: none;
       }
     }
-    & button {
+    & .button {
+      display: none;
+      ${mediaScreen(1280)} {
+        margin-left: auto;
+        display: block;
+      }
+    }
+    & .burgerButton {
+      background-color: transparent;
+      border: none;
       margin-left: auto;
+      ${mediaScreen(1280)} {
+        display: none;
+      }
     }
   }
 `;
