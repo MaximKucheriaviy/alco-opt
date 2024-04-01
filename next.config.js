a13d37f630a61c348cc9618f92bb4487c426f901
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2024-04-01 14:00:52 +0300

    newtlify fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
 };
+
+// builde netlify fix
 
 module.exports = nextConfig;
